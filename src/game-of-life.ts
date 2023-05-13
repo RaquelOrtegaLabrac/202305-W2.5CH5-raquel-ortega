2d24bdf6c20beda60b4e72ff3bf7e0abb8f5c3aa
labrac <raquelortegalabrac@hotmail.com>
2023-05-13 14:38:22 +0200

    Add tsconfig

---
+++ src/game-of-life.ts
@@ -0,0 +1,12 @@
+const rows = 4;
+const columns = 3;
+const array = [];
+
+const createBoard = (rows, columns) => {
+  for (let index = 0; index < columns; index++) {
+    array[index];
+    for (let j = 0; index < rows; index++) {
+      array[0];
+    }
+  }
+};
