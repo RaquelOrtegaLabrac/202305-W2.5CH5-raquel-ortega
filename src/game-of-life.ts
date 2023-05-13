45584075e9fbf6703b54e1a772a2fb16ef0c78c4
labrac <raquelortegalabrac@hotmail.com>
2023-05-13 15:36:36 +0200

    CreateBoard function working

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,10 +1,8 @@
-const rows = 4;
-const columns = 3;
-const arrayOfArrays: number[][] = [];
-
 const createBoard = (rows: number, columns: number) => {
+  const arrayOfArrays: number[][] = [];
+
   for (let index = 0; index < columns; index++) {
-    arrayOfArrays[index];
+    arrayOfArrays[index] = [];
     for (let j = 0; j < rows; j++) {
       arrayOfArrays[index][j] = Math.floor(Math.random() * 2);
     }
