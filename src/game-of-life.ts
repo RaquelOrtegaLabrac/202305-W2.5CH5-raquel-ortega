588177c443c5591476ea73bebb24898a683052df
labrac <raquelortegalabrac@hotmail.com>
2023-05-13 15:12:41 +0200

    Add createBoard function

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,12 +1,15 @@
 const rows = 4;
 const columns = 3;
-const array = [];
+const arrayOfArrays: number[][] = [];
 
-const createBoard = (rows, columns) => {
+const createBoard = (rows: number, columns: number) => {
   for (let index = 0; index < columns; index++) {
-    array[index];
-    for (let j = 0; index < rows; index++) {
-      array[0];
+    arrayOfArrays[index];
+    for (let j = 0; j < rows; j++) {
+      arrayOfArrays[index][j] = Math.floor(Math.random() * 2);
     }
   }
+  console.log(arrayOfArrays);
 };
+
+createBoard(4, 3);
